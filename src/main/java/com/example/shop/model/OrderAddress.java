3ba3f251acaecfbc98f0b07477ef8bd242eb238a
Basawaraj15 <103356103+Basawaraj15@users.noreply.github.com>
2024-10-02 15:18:56 +0530

    Update OrderAddress.java

--- src/main/java/com/example/shop/model/OrderAddress.java
+++ src/main/java/com/example/shop/model/OrderAddress.java
@@ -10,111 +10,31 @@ import lombok.Data;
 @Entity
 public class OrderAddress {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	private String Name;
+    private String name;
+    private String email;
+    private String mobileNo;
+    private String address;
+    private String city;
+    private String state;
+    private String pincode;
 
+    // Lombok @Data will handle getters, setters, toString, equals, and hashCode
 
-	private String email;
-
-	
-
-	public OrderAddress(Integer id, String name, String email, String mobileNo, String address, String city,
-			String state, String pincode) {
-		super();
-		this.id = id;
-		Name = name;
-		this.email = email;
-		this.mobileNo = mobileNo;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.pincode = pincode;
-	}
-
-	@Override
-	public String toString() {
-		return "OrderAddress [id=" + id + ", Name=" + Name + ", email=" + email + ", mobileNo=" + mobileNo
-				+ ", address=" + address + ", city=" + city + ", state=" + state + ", pincode=" + pincode + "]";
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return Name;
-	}
-
-	public void setName(String name) {
-		Name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getMobileNo() {
-		return mobileNo;
-	}
-
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getPincode() {
-		return pincode;
-	}
-
-	public void setPincode(String pincode) {
-		this.pincode = pincode;
-	}
-
-	private String mobileNo;
-
-	private String address;
-
-	private String city;
-
-	private String state;
-
-	private String pincode;
-
-	 
-	
-
+    // Constructor
+    public OrderAddress(Integer id, String name, String email, String mobileNo, String address, 
+                        String city, String state, String pincode) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobileNo = mobileNo;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.pincode = pincode;
+    }
 }
+
